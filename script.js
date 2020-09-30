6f93afe638de6eac837bf0807cee508cabe7eedc
Charlie Howard <chuckhoward99@gmail.com>
2020-09-30 10:23:26 -0600

    updated files

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
+//my questions 
+var questions = [{
+  question: "What sports franchise holds the record for most consecutive playoff wins?",
+  answers: ["Chicago Bulls", "Los Angeles Lakers", "New York Islanders","New York Yankees"],
+  correctAnswer: "New York Islanders",
+  
+}, {
+  question: "Who cut the cut ring from Sauron's hand?",
+  answers: ["Frodo", "Isildur", "Gimli", "Gandalf"],
+  correctAnswer: "Isildur",
+  
+}, {
+question: "Who was Bruce Lee's master?",
+  answers: ["Jackie Chan", "Ip Man", "Wong Fei-Hung", "Sammo Hung"],
+  correctAnswer: "Ip Man",
+  
+},
 
-var myQuestions = [
-  new question("What sport's franchise hold the record for most consecutive playoff wins in history?", [  "New York Yankees", "Chicago Bulls", "Los Angeles Lakes","New York Islanders"], "New York Islanders"),
-  new question("Which movie at the Oscar's was the first and only movie to win in every category that it was nominated for?", [ "The Lord of the Rings: The Return Of The King", "Gone With The Wind", "Schindler's List", "Titanic"], "The Lord Of The Rings: The Return Of The King"),
-  new question("Which is not a blink-182 song?", [ "First Date", "Feeling This", "Kiss and Tell", "The Rock Show"], "Kiss and Tell"),
-  new question("In the acclaimed movie the Goonies what was Chunk's real name?", [ "Chuck", "Lawrence", "Jake", "Francis"], "Lawrence")
-]
-var time = 60
+];
+//WHEN I click the start button
+var startButton = document.getElementById("startBtn")
 
+function initialize() {
+  startButton.style.display = "Start"
+}
+//THEN a timer starts and I am presented with a question
+var timeEl = document.querySelector("Time")
+var secondsLeft = 60
 
-
-
-function starTime() {
-  var myTimer =  setInterval(function () {
-    time --;
-    if (time === 0) {
+function startTime () {
+  var myTimer = setInterval(function (){
+    secondsLeft --;
+    if (secondsLeft === 0) {
       clearInterval(myTimer)
     }
   })
-
 }
 
+function firstQuestion () {
+  alert("first question!")
 
+}
+//WHEN I answer a question
+if(firstQuestion !== )
 
+//THEN I am presented with another question
+//WHEN I answer a question incorrectly
+//THEN time is subtracted from the clock
+//WHEN all questions are answered or the timer reaches 0
+//THEN the game is over
+//WHEN the game is over
+//THEN I can save my initials and score
