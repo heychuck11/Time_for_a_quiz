eaa6719133fb1f17314a2e24e4ebc6d10620cb59
Charlie Howard <chuckhoward99@gmail.com>
2020-09-30 21:56:28 -0600

    updated files

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //my questions 
-var questions = [{
+var questions = [
+  {
   question: "What sports franchise holds the record for most consecutive playoff wins?",
   answers: ["Chicago Bulls", "Los Angeles Lakers", "New York Islanders","New York Yankees"],
   correctAnswer: "New York Islanders",
@@ -17,31 +18,86 @@ question: "Who was Bruce Lee's master?",
 },
 
 ];
-//WHEN I click the start button
-var startButton = document.getElementById("startBtn")
 
-function initialize() {
-  startButton.style.display = "Start"
-}
-//THEN a timer starts and I am presented with a question
-var timeEl = document.querySelector("Time")
-var secondsLeft = 60
+var questionIdx = 0;
+var secondsLeft = 60;
+var currentScore = 0;
 
-function startTime () {
+//WHEN I click the start button, load the first question
+var startButton = document.createElement("button");
+startButton.innerHTML ="Start Quiz";
+
+
+  function startQuiz(){
+    startButton.addEventListener("click" + questionIdx)
+  }
+document.body.appendChild(startButton)
+
+
+
+
+console.log("Start")
+
+// Event listener for start button; when button is clicked start timer 
+
+
+
+//Uses set interval to start the timer
+function startTimer(){
   var myTimer = setInterval(function (){
     secondsLeft --;
     if (secondsLeft === 0) {
       clearInterval(myTimer)
     }
-  })
+  }, 1000);
+  displayQuestion();
 }
 
-function firstQuestion () {
-  alert("first question!")
+// Gets the current question index and displays question & answers on the screen
+function displayQuestion(){
+  // Display the question by getting the currenr question index
+  // and pull that object from the array
+  var questionObj = questions[questionIdx];
+
+  // Display question title
+
+  // Loop through the answers
+     // Create a button for each answer
+     // Check if the button being created is the correct answer or not
+     // Create a custom attribute to show if it's correct or not
+
+  // Append everything to the page
 
 }
+
+// Create an event listener for when any answer is clicked
+// Listener should also see if the answer is correct
+
+function answerIsCorrect(){
+
+
+  questionIdx++
+  displayQuestion();
+}
+
+function answerIsWrong(){
+
+  questionIdx++;
+  displayQuestion();
+}
+
+
+function initialize() {
+  startButton.style.display = "Start"
+}
+//THEN a timer starts and I am presented with a question
+
+
+
+
+
+
 //WHEN I answer a question
-if(firstQuestion !== )
 
 //THEN I am presented with another question
 //WHEN I answer a question incorrectly
